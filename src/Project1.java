f8d2dea108500ada817027c2b0933bf80c6e8250
Mike Baldwin <mikebaldwin21@gmail.com>
2018-10-26 21:12:40 -0400

    Added comments.

--- src/Project1.java
+++ src/Project1.java
@@ -25,11 +25,11 @@ public class Project1 {
 	 */
 	private static int groupSize;
 	/**
-	 * The first group of people that are to paired to the second group.
+	 * The first group of people that are to be paired to the second group.
 	 */
 	private static List<Person> groupA = new ArrayList<>();
 	/**
-	 * The second group of people that are to paired to the first group.
+	 * The second group of people that are to be paired to the first group.
 	 */
 	private static List<Person> groupB = new ArrayList<>();
 	/**
@@ -63,30 +63,57 @@ public class Project1 {
 		for (Person iPerson : groupB)
 			iPerson.convertAndSort(groupA);
 
-		//Now on to the actual algorithm(s)
+
+		/* Now on to the actual algorithms.
+		The first uses backtracking to find a stable pairing for all people.
+		NOTE: I don't need to check for stability because the Gale-Shapely algorithm GUARANTEES a stable solution
+		and the backtracking algorithm will, at worst, run every single possible solution. Which means that it will
+		eventually find the Gale-Shapely solution, hence it WILL find a stable solution.
+		 The process is a follows:
+		  1. Loop over the people in the first group ,they are the proposers.
+		  2. For each person, if they are NOT already paired, then start looping over their preferences (They are already in order).
+		  3. For each preference:
+		        a. If the pref is unpaired, then pair the person and their preference and push that pair to the stack.
+		        b. If the pref is already paired, we have to backtrack.
+		        ONLY unpair the person, reset their index counter (needed for starting at their top pref again),
+		        unpair the previous person (top of the stack, which would be the last step),
+		        pop the top of the stack, and move two steps back in the person counter to try again.
+
+		The second algorithm is the Gale-Shapely algorithm found at <https://en.wikipedia.org/wiki/Stable_marriage_problem>.
+			It's simple, fast, and specific to the stable marriage problem.
+		  */
+
+		//Backtracking -- START
 		if (useBackTracking) {
 			for (int i = 0; i < groupSize; i++) {
 				Person iPerson = groupA.get(i);
 				while (!iPerson.isPaired()) {
 					Person iPref = iPerson.getNextPref();
 					if (!iPerson.preferOverCurrent(iPref) || !iPref.preferOverCurrent(iPerson))
-						continue;
+						continue; //No need to check pairing with this pref, they hate each other :)
 					if (!iPref.isPaired())
 						pairings.push(new Pairing(iPerson, iPref));
 					else {
-						iPerson.pairWith(null);
+						iPerson.pairWith(null); //Unpair
 						iPerson.resetPrefIndex();
-						pairings.top().personA.pairWith(null);
+						pairings.top().personA.pairWith(null); //Unpair
 						pairings.pop();
 						i -= 2;
-						break;
+						break; //Break out of while loop
 					}
 				}
 			}
-			for (Pairing iPair : pairings.getRawData())
-				System.out.println(iPair.personA.getName() + " - " + iPair.personB.getName());
-		} else {
-			while (!areMarriagesStable()) {
+
+			//Print off the pairs.
+			for (int i = 0; i < pairings.getRawData().size(); i++) {
+				Pairing iPair = pairings.getRawData().get(i);
+				System.out.println("Team " + i + ": " + iPair.personA.getName() + " and " + iPair.personB.getName());
+				}
+		} //Backtracking -- END
+
+		//Gale-Shapely Algorithm -- START
+		else {
+			while (!isEveryonePaired()) {
 				for (Person iPerson : groupA) {
 					for (Person iPref : iPerson.getSortedPrefs()) {
 						if (!iPerson.isPaired()) {
@@ -99,21 +126,22 @@ public class Project1 {
 				}
 			}
 
-			for (Person iPerson : groupA) {
-				System.out.println(iPerson.getName() + " - " + iPerson.getCurrentFiance().getName());
+			//Print off the pairs.
+			for (int i = 0; i < groupA.size(); i++) {
+				Person iPerson = groupA.get(i);
+				System.out.println("Team " + i + ": " + iPerson.getName() + " and " + iPerson.getCurrentFiance().getName());
 			}
-		}
+		} //Gale-Shapely -- END
 	}
 
 	/**
-	 * Have we achieved a stable marriage?
-	 * NOTE: Because I'm using the Gale-Shapely algorithm, it is guaranteed to make a stable pairing, so there is no need to do actual stability checking.
+	 * The Gale-Shapely algorithm is guaranteed to find a stable solution, so there is no need to do actual stability checking.
 	 * Instead I'm just checking to see if everyone has paired off, if so, it is stable.
 	 * NOTE 2: Using this method, the algorithm only takes 6 steps to find a stable solution with the provided TestData file.
 	 *
 	 * @return True if pairing is finished, false otherwise.
 	 */
-	private static boolean areMarriagesStable() {
+	private static boolean isEveryonePaired() {
 		for (Person iPerson : groupA)
 			if (!iPerson.isPaired())
 				return false;
